88dd42654d52fc7c9b3b8afaa5954cc1b5adb9f3
plutos28 <gamevictor28@gmail.com>
2020-11-16 14:07:27 +0300

    add model for user and add to users model

---
+++ src/app/user.ts
@@ -0,0 +1,22 @@
+// This model represents the user information provided by github
+export interface User {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  folowing_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+  score: number;
+}
--- src/app/users.ts
+++ src/app/users.ts
@@ -1,5 +1,8 @@
+import { User } from './user';
+
+// This model represents what a request for users returns
 export interface Users {
   total: number;
   incomplete_results: boolean;
-  items: any[];
+  items: User[];
 }
