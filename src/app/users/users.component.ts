6a85674d9bf7a6678c0368180db7a778f76257db
plutos28 <gamevictor28@gmail.com>
2020-11-16 23:13:30 +0300

    show the results after submitting the search form

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {GithubService} from '../github.service';
+import {SearchService} from '../search.service';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
+  totalCount;
+  constructor(
+    private githubService: GithubService,
+    private searchService: SearchService
+  ) { }
 
   ngOnInit(): void {
+    this.githubService.getUsers(this.searchService.get()).subscribe({
+      next: value => {
+        this.totalCount = value.total_count;
+      }
+    });
   }
 
 }
