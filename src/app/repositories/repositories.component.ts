01a75c80872ea7ed9004719ca13f6f4c9dce44b5
plutos28 <gamevictor28@gmail.com>
2020-11-17 07:02:02 +0300

    subscribe to the users in repositories

--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../user';
+import {GithubService} from '../github.service';
+import {SearchService} from '../search.service';
 
 @Component({
   selector: 'app-repositories',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repositories.component.css']
 })
 export class RepositoriesComponent implements OnInit {
-
-  constructor() { }
+  totalCount: number;
+  users: User[];
+  constructor(
+    private githubService: GithubService,
+    private searchService: SearchService
+  ) { }
 
   ngOnInit(): void {
-  }
+    this.githubService.getUsers(this.searchService.get()).subscribe({
+      next: value => {
+        this.totalCount = value.total_count;
+      }
+    });
 
+    this.githubService.getUsersFromPage(this.searchService.get(), 1).subscribe({
+      next: value => {
+        this.users = value.items;
+        console.log(this.users);
+      }
+    });
+  }
 }
