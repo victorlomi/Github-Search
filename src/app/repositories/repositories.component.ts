d5708d688227b3df03b66d19e9eb964a0e06736b
plutos28 <gamevictor28@gmail.com>
2020-11-17 07:53:54 +0300

    inject text from repositories into html

--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -3,6 +3,7 @@ import {User} from '../user';
 import {GithubService} from '../github.service';
 import {SearchService} from '../search.service';
 import {Repositories} from '../repositories';
+import {Repository} from '../repository';
 
 @Component({
   selector: 'app-repositories',
@@ -11,7 +12,7 @@ import {Repositories} from '../repositories';
 })
 export class RepositoriesComponent implements OnInit {
   totalCount: number;
-  users: Repositories[];
+  repositories: Repository[];
   constructor(
     private githubService: GithubService,
     private searchService: SearchService
@@ -21,13 +22,7 @@ export class RepositoriesComponent implements OnInit {
     this.githubService.getRepositories(this.searchService.get()).subscribe({
       next: value => {
         this.totalCount = value.total_count;
-      }
-    });
-
-    this.githubService.getUsersFromPage(this.searchService.get(), 1).subscribe({
-      next: value => {
-        this.users = value.items;
-        console.log(this.users);
+        this.repositories = value.items;
       }
     });
   }
